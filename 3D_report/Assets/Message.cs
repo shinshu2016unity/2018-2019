0f9dc35275337e0fcd6f92cb3a03296f2a53974e
Rtsutsumi <16t2108g@shinshu-u.ac.jp>
2018-12-26 14:29:07 +0900

    first commit

---
+++ 3D_report/Assets/Message.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message : MonoBehaviour {
+    //public string textToEdit = "Textfield";	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public string textToEdit = "メッセージを入力";
+void OnGUI()
+    {
+        // テキストエリアを表示する
+        textToEdit = GUI.TextArea(new Rect(10, 10, 250, 100), textToEdit);
+    }
+}
