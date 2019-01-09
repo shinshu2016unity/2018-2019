34fc53796ec03055169fb3e1ac43c8a6942ee638
ahyakashimoto <bb71424@gmail.com>
2019-01-09 14:29:27 +0900

    stage1

---
+++ CreateGame/Assets/BeltConveir.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltConveir : MonoBehaviour {
+    [SerializeField]
+    private float speed = 1f;
+    public Vector3 ConveyorVelocity()
+    {
+        return transform.forward * speed;
+    }
+
+
+    // Use this for initialization
+    void Start() {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
