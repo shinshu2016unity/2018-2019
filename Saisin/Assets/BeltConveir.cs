edc119d0aa75c338da87310aac049826bbda9ff8
16t2109e <16t2109e@shinshu-u.ac.jp>
2019-01-16 18:00:46 +0900

    saisinban

---
+++ Saisin/Assets/BeltConveir.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltConveir : MonoBehaviour {
+    [SerializeField]
+    private float speed = 1f;    //速度設定
+    public Vector3 ConveyorVelocity()
+    {
+        return transform.forward * speed;
+    }
+
+
+    // Use this for initialization
+    void Start() {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Saisin/Assets/cameramove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramove : MonoBehaviour
+{
+    private GameObject Subcamera;
+    // Use this for initialization
+    void Start()
+    {
+        Subcamera = GameObject.Find("Subcamera");
+        
+    }
+
+    // Update is called once per frame
+    public Camera cam;
+    void Update()
+    {
+        if(transform.position.y < 90)
+        { 
+            transform.Translate(new Vector3(0, 0.1f, 0)); //上昇
+            transform.Rotate(new Vector3(0, 1f, 0));//回転
+        }
+        else
+        {
+            Subcamera.SetActive(false);
+        }
+
+    }
+}
